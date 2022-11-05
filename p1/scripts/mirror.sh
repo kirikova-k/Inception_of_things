ac75899248a7e89e4257434f844763067db4b746
user <ogarthar@student.21-school.ru>
2022-11-05 21:12:36 +0300

    20.04

--- p1/scripts/mirror.sh
+++
@@ -1,4 +0,0 @@
-sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
-sudo yum update -y
-yum install net-tools -y #ifconfig install
