0b0bda82979ac1faa1bef3a7bbdef58d9f7152f8
user <ogarthar@student.21-school.ru>
2022-11-06 21:46:12 +0300

    p1 done

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -2,6 +2,6 @@ curl -sfL https://get.k3s.io | sh -s - server \
 --write-kubeconfig-mode 0644 \
 --node-ip "192.168.42.110" \
 --flannel-iface eth1
-# sudo apt-get install virtualbox-guest-additions-iso
+sudo apt-get install virtualbox-guest-additions-iso
 
 cat /var/lib/rancher/k3s/server/node-token > /vagrant/token
