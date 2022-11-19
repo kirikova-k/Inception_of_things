c48bce4433a29604b331b34870fc78f573fb866c
user <ogarthar@student.21-school.ru>
2022-11-19 16:31:44 +0300

    p3 bbbb

--- p3/scripts/start-vm.sh
+++ p3/scripts/start-vm.sh
@@ -2,4 +2,4 @@ cd ../.
 sudo vagrant up
 cd -
 ansible-galaxy install -r ./ansible/requirements.yaml
-ansible-playbook -i ./ansible/inventory/host.ini ./ansible/all.yaml
+ansible-playbook -i ./ansible/inventory/host.ini ./ansible/all.yaml --ask-pass
